4eb212a9835a110139aa1f76d8957e334b203b49
Pete P <peter.pirkelbauer@gmx.at>
2018-09-22 12:43:41 -0500

    added missing files

---
+++ examples/tasks/common/qthreads.hpp
@@ -0,0 +1,38 @@
+
+#ifndef COMMON_QTHREADS_HPP
+#define COMMON_QTHREADS_HPP 1
+
+#include <sstream>
+
+template <class V>
+static inline
+void qthr_setenv(std::string name, V val)
+{
+  std::stringstream str;
+
+  str << name << "=" << val;
+
+  char* envset = new char[str.str().size()+1];
+
+  memcpy(envset, str.str().c_str(), str.str().size()+1);
+  putenv(envset);
+
+  delete[] envset;
+}
+
+static inline
+void init_qthreads(size_t numthreads, size_t stacklen = 0)
+{
+  assert(numthreads);
+
+  size_t num_shepherds = (numthreads+4) / 5;
+
+  qthr_setenv("QTHREAD_HWPAR", numthreads);
+
+  if (stacklen) qthr_setenv("QTHREAD_STACK_SIZE", stacklen);
+
+  // qthread_initialize();
+  qthread_init(num_shepherds);
+}
+
+#endif /* COMMON_QTHREADS_HPP */
---
+++ scripts/lockConfigs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+test_make()
+{
+  lock=$1
+  expected=$2
+
+  make TEST_LOCK="$lock" -f Makefile.locks
+  res="$?"
+
+  if [[ "$expected" -ne "$res" ]]; then
+    exit $res
+  fi
+
+  if [[ "$expected" -eq 0 ]]; then
+    echo run
+    mv "$COMPDIR"/lockTest.bin "$COMPDIR"/"lock-$lock".bin
+  fi
+}
+
+test_locks()
+{
+  test_make TEST_ANDERSON 0
+  test_make TEST_COUNTING 0
+  test_make TEST_MCS 0
+  test_make TEST_CLH 0
+  test_make TEST_TTAS 0
+  test_make TEST_TTAS_BO 0
+  test_make TEST_MUTEX 0
+}
+
+
+###
+# COMPILERS
+
+# GNU compilers
+# COMPILERS="g++-4.8 g++-4.9 g++-5.0 powerpc-linux-gnu-g++-4.9 arm-linux-gnueabihf-g++-4.9 x86_64-w64-mingw32-g++"
+
+# Clang family
+# COMPILERS="$COMPILERS clang++-3.4 clang++-3.5 clang++-3.6 clang++-3.7"
+
+# Intel compiler
+# COMPILERS="$COMPILERS icpc"
+
+
+###
+# LOCK TESTS
+test_locks
+
